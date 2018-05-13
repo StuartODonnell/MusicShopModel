61c20665e387b4c5a50b363de1b099805ff20416
stooggle <stuartodonnell@gmail.com>
2018-05-14 00:19:36 +0100

    Adds Trumpet Class tests.

--- src/main/java/instruments/Trumpet.java
+++ src/main/java/instruments/Trumpet.java
@@ -1,4 +1,24 @@
 package instruments;
 
-public class Trumpet {
+public class Trumpet extends Instrument{
+
+    public int noOfValves;
+
+    public Trumpet(String description, double buyPrice, double sellPrice, InstrumentType instrumentType, String colour, String material, String make, String model, int noOfValves) {
+        super(description, buyPrice, sellPrice, instrumentType.BRASS, colour, material, make, model);
+        this.noOfValves = noOfValves;
+    }
+
+    public int getNoOfValves() {
+        return noOfValves;
+    }
+
+    public void setNoOfValves(int noOfValves) {
+        this.noOfValves = noOfValves;
+    }
+
+    public String play(){
+        return "parp....parpy...purp";
+
+    }
 }

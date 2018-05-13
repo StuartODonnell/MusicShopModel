17a833881f6e5e86186848cfa9db49df10a993dc
stooggle <stuartodonnell@gmail.com>
2018-05-14 00:07:52 +0100

    Adds Timpani Class tests.

--- src/main/java/instruments/Timpani.java
+++ src/main/java/instruments/Timpani.java
@@ -1,4 +1,24 @@
 package instruments;
 
-public class Timpani {
+public class Timpani extends Instrument {
+
+    public int noOfDrums;
+
+    public Timpani(String description, double buyPrice, double sellPrice, InstrumentType instrumentType, String colour, String material, String make, String model, int noOfDrums) {
+        super(description, buyPrice, sellPrice, instrumentType.PERCUSSION, colour, material, make, model);
+        this.noOfDrums = noOfDrums;
+    }
+
+    public int getNoOfDrums() {
+        return noOfDrums;
+    }
+
+    public void setNoOfDrums(int noOfDrums) {
+        this.noOfDrums = noOfDrums;
+    }
+
+    public String play(){
+        return "boom....boomy...bam";
+
+    }
 }
